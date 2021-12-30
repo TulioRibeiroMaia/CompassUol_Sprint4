f204c1f7eca91889a319359bef31e5a366886f9e
TulioRibeiroMaia <tulio.ribeiromaia@gmail.com>
2021-12-29 22:37:28 -0300

    Terminando os endpoints

--- src/test/java/Avaliacao/Sprint4/PartidosPoliticos/Controller/AssociatesControllerTest.java
+++ src/test/java/Avaliacao/Sprint4/PartidosPoliticos/Controller/AssociatesControllerTest.java
@@ -1,22 +1,13 @@
 package Avaliacao.Sprint4.PartidosPoliticos.Controller;
 
-import Avaliacao.Sprint4.PartidosPoliticos.Entity.Associates;
-import Avaliacao.Sprint4.PartidosPoliticos.config.Handler.ObjectMapperConfig;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-
-import java.net.URI;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
